1c492a26fc3d6bc5ca43a28100080f0cf9ee28ca
NethePaul <69491224+NethePaul@users.noreply.github.com>
2021-07-26 12:24:18 +0200

    compile.bat now uses the %CATALINA_HOME% enviornment variable instead of an absolute path

--- WEB-INF/classes/compile.bat
+++ WEB-INF/classes/compile.bat
@@ -1,1 +1,1 @@
-javac -cp C:\Users\Admin\Downloads\apache-tomcat-9.0.50/lib/* Servlet.java encryption.java Session.java database.java 
+javac -cp %CATALINA_HOME%/lib/* Servlet.java encryption.java Session.java database.java 
